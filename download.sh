51770855a934c415e81b65a2469c8431e97fcb7f
SBKarr <sbkarr@stappler.org>
2020-11-22 03:42:32 -0800

    iOS: macOS 11 support

--- download.sh
+++ download.sh
@@ -30,44 +30,44 @@ function get_gif() {
 
 function get_webp() {
 	rm -rf libwebp
-	wget https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-1.0.2.tar.gz
-	tar -xzf libwebp-1.0.2.tar.gz
-	rm libwebp-1.0.2.tar.gz
-	mv libwebp-1.0.2 libwebp
+	wget https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-1.1.0.tar.gz
+	tar -xzf libwebp-1.1.0.tar.gz
+	rm libwebp-1.1.0.tar.gz
+	mv libwebp-1.1.0 libwebp
 }
 
 function get_brotli() {
 	rm -rf brotli
-	git clone https://github.com/google/brotli.git  --branch v1.0.7 --single-branch
+	git clone https://github.com/google/brotli.git  --branch v1.0.9 --single-branch
 }
 
 function get_mbedtls() {
 	rm -rf mbedtls
-	git clone https://github.com/ARMmbed/mbedtls.git --branch mbedtls-2.16.1 --single-branch
+	git clone https://github.com/ARMmbed/mbedtls.git --branch mbedtls-2.24.0 --single-branch
 }
 
 function get_curl() {
 	rm -rf curl
-	wget https://curl.haxx.se/download/curl-7.70.0.tar.gz
-	tar -xzf curl-7.70.0.tar.gz
-	rm curl-7.70.0.tar.gz
-	mv curl-7.70.0 curl
+	wget https://curl.se/download/curl-7.73.0.tar.gz
+	tar -xzf curl-7.73.0.tar.gz
+	rm curl-7.73.0.tar.gz
+	mv curl-7.73.0 curl
 }
 
 function get_freetype() {
 	rm -rf freetype
-	wget https://download.savannah.gnu.org/releases/freetype/freetype-2.10.1.tar.gz
-	tar -xzf freetype-2.10.1.tar.gz
-	rm freetype-2.10.1.tar.gz
-	mv freetype-2.10.1 freetype
+	wget https://download.savannah.gnu.org/releases/freetype/freetype-2.10.4.tar.gz
+	tar -xzf freetype-2.10.4.tar.gz
+	rm freetype-2.10.4.tar.gz
+	mv freetype-2.10.4 freetype
 }
 
 function get_sqlite() {
 	rm -rf sqlite
-	wget https://sqlite.org/2019/sqlite-amalgamation-3280000.zip
-	unzip sqlite-amalgamation-3280000.zip -d .
-	rm sqlite-amalgamation-3280000.zip
-	mv sqlite-amalgamation-3280000 sqlite
+	wget https://www.sqlite.org/2020/sqlite-amalgamation-3330000.zip
+	unzip sqlite-amalgamation-3330000.zip -d .
+	rm sqlite-amalgamation-3330000.zip
+	mv sqlite-amalgamation-3330000 sqlite
 }
 
 function get_glfw() {
@@ -79,10 +79,10 @@ function get_glfw() {
 }
 
 function get_libidn2 {
-	wget ftp://ftp.gnu.org/gnu/libidn/libidn2-2.2.0.tar.gz
-	tar -xzf libidn2-2.2.0.tar.gz
-	rm libidn2-2.2.0.tar.gz
-	mv libidn2-2.2.0 libidn2
+	wget ftp://ftp.gnu.org/gnu/libidn/libidn2-2.3.0.tar.gz
+	tar -xzf libidn2-2.3.0.tar.gz
+	rm libidn2-2.3.0.tar.gz
+	mv libidn2-2.3.0 libidn2
 }
 
 get_jpeg
